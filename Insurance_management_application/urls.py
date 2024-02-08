d555e69cd45ae1a25a86452ed409713ca608822b
grzegorzkoniuszewski <koniuszewski.grzegorz@gmail.com>
2024-02-08 18:41:22 +0100

    Changes related in authentication.

--- Insurance_management_application/urls.py
+++ Insurance_management_application/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('accounts/login/', LoginView.as_view(), name='login'),
 ]
---
+++ accounts/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone_number', 'postal_code', 'location')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ['id', 'username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff']
+    list_display_links = ['id', 'username', 'email', 'first_name', 'last_name']
+    list_filter = ['is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions']
+    search_fields = ['username', 'email', 'first_name', 'last_name', 'phone_number', 'postal_code', 'location']
+    ordering = ['id']
