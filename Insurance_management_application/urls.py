9950ef7f69b34058790353e7a95e0935b9569c42
grzegorzkoniuszewski <koniuszewski.grzegorz@gmail.com>
2024-02-19 21:20:07 +0100

    Added urls.py file in ima_display app and modify settings.py in main project and urls.

--- Insurance_management_application/urls.py
+++ Insurance_management_application/urls.py
@@ -17,10 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from ima_display import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
     # path('accounts/login/', LoginView.as_view(), name='login'),
 ]
