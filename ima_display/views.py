cb7ffde0bcef2a7bcb02ac970f9ddf22a2b14c20
mackul1988 <mackul1988@users.noreply.github.com>
2024-02-20 14:11:23 +0100

    Creating basic view for main page

--- ima_display/views.py
+++ ima_display/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+# def hello(request):
+#     return HttpResponse('Witaj w aplikacji do zarządzania polisami samochodowymi')
+
+
+class StartPage(TemplateView):
+    template_name = 'index.html'
 
 # Create your views here.
