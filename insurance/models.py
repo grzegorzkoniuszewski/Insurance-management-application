4baf420d08cf5a15a10ca8cce2233b256c90848d
grzegorzkoniuszewski <koniuszewski.grzegorz@gmail.com>
2024-02-26 19:30:11 +0100

    Restore modification in models.py in insurance app.

--- insurance/models.py
+++ insurance/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from datetime import timedelta
 
 
 class PolicyStatus(models.Model):
@@ -9,10 +8,6 @@ class PolicyStatus(models.Model):
     def __str__(self):
         # Returns the name of the policy status as a string representation of the object.
         return self.policy_status_name
-
-    class Meta:
-        verbose_name = "Policy status"
-        verbose_name_plural = "Policy status"
 
 
 class PolicyType(models.Model):
@@ -24,7 +19,6 @@ class PolicyType(models.Model):
         # Returns the name of the policy type as a string representation of the object.
         return self.policy_type_name
 
-
 class InsurancePolicy(models.Model):
     # Represents an insurance policy.
     # Policy number, unique for each policy.
@@ -32,29 +26,21 @@ class InsurancePolicy(models.Model):
     # Start date of policy validity.
     validity_start_date = models.DateField()
     # End date of policy validity.
-    validity_end_date = models.DateField(editable=False)  # Making the field non-editable in admin
+    validity_end_date = models.DateField()
     # Status of the policy, linked to PolicyStatus model, indicates the current state of the policy.
     status = models.ForeignKey(
         PolicyStatus,
-        verbose_name="Policies Status",
+        verbose_name="Policy Status",
         on_delete=models.SET_NULL,  # Action to take when related object is deleted.
         null=True,  # Allows null values in the database.
     )
     # Type of the policy, linked to PolicyType model, specifies the type of the policy.
     policy_type = models.ForeignKey(
         PolicyType,
-        verbose_name="Policies Type",
+        verbose_name="Policy Type",
         on_delete=models.SET_NULL,  # Action to take when related object is deleted.
         null=True,  # Allows null values in the database.
     )
-
-    def save(self, *args, **kwargs):
-        # Calculate the end date of policy validity to be one year minus one day from the start date
-        leap_year = self.validity_start_date.year % 4 == 0 and (
-                    self.validity_start_date.year % 100 != 0 or self.validity_start_date.year % 400 == 0)
-        days_in_year = 366 if leap_year else 365
-        self.validity_end_date = self.validity_start_date + timedelta(days=days_in_year - 1)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         # Returns the policy number as a string representation of the object.
