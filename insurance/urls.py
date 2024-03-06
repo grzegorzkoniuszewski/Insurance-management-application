2fb8a65783338d21c5038b9592ce16e0b3aa42b9
grzegorzkoniuszewski <koniuszewski.grzegorz@gmail.com>
2024-03-06 21:20:12 +0100

    Added insurance_delete.html template for deletion confirmation. Modified insurance_detail.html for delete button (commented out for testing).

--- insurance/urls.py
+++ insurance/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('insurance_add/', views.insurance_add, name="insurance_add"),
     path('insurance_list/', views.insurance_list, name="insurance_list"),
     path('insurance_detail/<int:policy_id>/', views.insurance_detail, name="insurance_detail"),
+    # path('insurance_delete/<int:policy_id>/', views.insurance_delete, name="insurance_delete"),
 ]
--- insurance/views.py
+++ insurance/views.py
@@ -1,39 +1,82 @@
+# from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+# from .models import InsurancePolicy
+# from .forms import InsuranceForm
+#
+#
+# def insurance_list(request):
+#
+#     queryDataAll = InsurancePolicy.objects.all()
+#
+#     context = {'AllInsurances': queryDataAll}
+#
+#     return render(request, 'insurance_list.html', context)
+#
+#
+# # View for displaying details of a single insurance policy.
+# def insurance_detail(request, policy_id):
+#     policy = InsurancePolicy.objects.get(pk=policy_id)
+#     return render(request, 'insurance_detail.html', {'policy': policy})
+#
+#
+# def insurance_add(request):
+#     form = InsuranceForm()
+#
+#     if request.method == 'POST':
+#
+#         form = InsuranceForm(request.POST)
+#
+#         if form.is_valid():
+#             form.save()
+#             return redirect('insurance_list')
+#         # else
+#     context = {"InsuranceForm": form}
+#
+#     return render(request, 'insurance_add.html', context)
+#
+#
+# def insurance_edit(request, policy_id):
+#     policy = InsurancePolicy.objects.get(pk=policy_id)
+#     if request.method == 'POST':
+#         # Process form data and update the policy.
+#         # Redirect to the policy detail page.
+#         pass
+#     else:
+#         return render(request, 'insurance_edit.html', {'policy': policy})
+#
+#
+# def insurance_delete(request, policy_id):
+#     policy = InsurancePolicy.objects.get(pk=policy_id)
+#     # Delete the policy and redirect to the policy list page.
+#     pass
+#
+#
+# def home(request):
+#     return render(request, 'base.html')
+
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import InsurancePolicy
 from .forms import InsuranceForm
 
-
 def insurance_list(request):
-
     queryDataAll = InsurancePolicy.objects.all()
-
     context = {'AllInsurances': queryDataAll}
-
     return render(request, 'insurance_list.html', context)
 
-
-# View for displaying details of a single insurance policy.
 def insurance_detail(request, policy_id):
     policy = InsurancePolicy.objects.get(pk=policy_id)
     return render(request, 'insurance_detail.html', {'policy': policy})
 
-
 def insurance_add(request):
     form = InsuranceForm()
-
     if request.method == 'POST':
-
         form = InsuranceForm(request.POST)
-
         if form.is_valid():
             form.save()
             return redirect('insurance_list')
-        # else
     context = {"InsuranceForm": form}
-
     return render(request, 'insurance_add.html', context)
-
 
 def insurance_edit(request, policy_id):
     policy = InsurancePolicy.objects.get(pk=policy_id)
@@ -45,10 +88,15 @@ def insurance_edit(request, policy_id):
         return render(request, 'insurance_edit.html', {'policy': policy})
 
 
-def insurance_delete(request, policy_id):
-    policy = InsurancePolicy.objects.get(pk=policy_id)
-    # Delete the policy and redirect to the policy list page.
-    pass
+# def insurance_delete(request, policy_id):
+#     policy = InsurancePolicy.objects.get(pk=policy_id)
+#     if request.method == 'POST':
+#         if request.POST.get('confirm_delete') == 'Yes':
+#             policy.delete()
+#             return redirect('insurance_list')
+#         else:
+#             return redirect('insurance_detail', policy_id=policy_id)
+#     return render(request, 'insurance_delete.html', {'policy': policy})
 
 
 def home(request):
