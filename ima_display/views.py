a04d9a621ed87b3267c00c9ad7e15709a95bae4f
grzegorzkoniuszewski <koniuszewski.grzegorz@gmail.com>
2024-03-07 19:31:02 +0100

    Modified mainpage.html and views.py in ima_display app.

--- ima_display/views.py
+++ ima_display/views.py
@@ -9,3 +9,9 @@ from django.views.generic import TemplateView
 
 class StartPage(TemplateView):
     template_name = 'mainpage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_user = self.request.user
+        context['current_user'] = current_user
+        return context
