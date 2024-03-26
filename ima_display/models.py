fc59190dac450769cdde443877cee13386060371
grzegorzkoniuszewski <koniuszewski.grzegorz@gmail.com>
2024-03-26 19:33:59 +0100

    Changes in Ima_display app.

--- ima_display/models.py
+++ ima_display/models.py
@@ -5,22 +5,15 @@ from django.views.generic import TemplateView
 # Widok StartPage oparty na klasie TemplateView
 
 class StartPage(TemplateView):
-    # Definicja klasy StartPage dziedziczącej po klasie TemplateView
 
     template_name = 'mainpage.html'
-    # Zdefiniowanie nazwy szablonu, który będzie renderowany przez ten widok
 
     def get_context_data(self, **kwargs):
-        # Metoda do pobierania kontekstu danych dla szablonu
 
         context = super().get_context_data(**kwargs)
-        # Pobranie kontekstu z klasy nadrzędnej
 
         current_user = self.request.user
-        # Pobranie informacji o aktualnym użytkowniku
 
         context['current_user'] = current_user
-        # Dodanie informacji o aktualnym użytkowniku do kontekstu
 
         return context
-        # Zwrócenie kontekstu danych
--- ima_display/views.py
+++ ima_display/views.py
@@ -1,17 +1,17 @@
 from django.views.generic import TemplateView
 
 class StartPage(TemplateView):
-    # Klasa widoku strony startowej dziedzicząca po klasie TemplateView
+
     template_name = 'mainpage.html'
-    # Ustawienie nazwy szablonu dla widoku
+
 
     def get_context_data(self, **kwargs):
-        # Metoda pobierająca dane kontekstowe
+
         context = super().get_context_data(**kwargs)
-        # Pobranie danych kontekstowych odziedziczonych z klasy nadrzędnej
+
         current_user = self.request.user
-        # Pobranie aktualnego użytkownika
+
         context['current_user'] = current_user
-        # Dodanie aktualnego użytkownika do danych kontekstowych
+
         return context
-        # Zwrócenie danych kontekstowych
+
