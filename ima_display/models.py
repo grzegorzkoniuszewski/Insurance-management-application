20b7744aff2ec964731310ed2b2cc7b00d1a0c13
grzegorzkoniuszewski <koniuszewski.grzegorz@gmail.com>
2024-03-26 19:32:28 +0100

    Changes in Ima_display app.

--- ima_display/models.py
+++ ima_display/models.py
@@ -1,13 +1,26 @@
-from django.db import models
-from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your models here.
+# Widok StartPage oparty na klasie TemplateView
 
+class StartPage(TemplateView):
+    # Definicja klasy StartPage dziedziczącej po klasie TemplateView
 
-class HomePageContent(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    created_at = models.DateTimeField(default=datetime.now)
+    template_name = 'mainpage.html'
+    # Zdefiniowanie nazwy szablonu, który będzie renderowany przez ten widok
 
-    def __str__(self):
-        return self.title
+    def get_context_data(self, **kwargs):
+        # Metoda do pobierania kontekstu danych dla szablonu
+
+        context = super().get_context_data(**kwargs)
+        # Pobranie kontekstu z klasy nadrzędnej
+
+        current_user = self.request.user
+        # Pobranie informacji o aktualnym użytkowniku
+
+        context['current_user'] = current_user
+        # Dodanie informacji o aktualnym użytkowniku do kontekstu
+
+        return context
+        # Zwrócenie kontekstu danych
--- ima_display/views.py
+++ ima_display/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import TemplateView
 
-
-# def hello(request):
-#     return HttpResponse('Witaj w aplikacji do zarządzania polisami samochodowymi')
-
-
 class StartPage(TemplateView):
+    # Klasa widoku strony startowej dziedzicząca po klasie TemplateView
     template_name = 'mainpage.html'
+    # Ustawienie nazwy szablonu dla widoku
 
     def get_context_data(self, **kwargs):
+        # Metoda pobierająca dane kontekstowe
         context = super().get_context_data(**kwargs)
+        # Pobranie danych kontekstowych odziedziczonych z klasy nadrzędnej
         current_user = self.request.user
+        # Pobranie aktualnego użytkownika
         context['current_user'] = current_user
+        # Dodanie aktualnego użytkownika do danych kontekstowych
         return context
+        # Zwrócenie danych kontekstowych
