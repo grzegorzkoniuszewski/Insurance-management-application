a4cb3157ed086db1901275e4e3599360cc964bab
grzegorzkoniuszewski <koniuszewski.grzegorz@gmail.com>
2024-03-27 12:17:09 +0100

    Changes in insurance app model.

--- insurance/models.py
+++ insurance/models.py
@@ -1,72 +1,60 @@
 from django.db import models
-from django.utils.text import slugify
 from TUBase.models import CustomTUBase
 from accounts.models import CustomUser
 
 
 class PolicyStatus(models.Model):
-    # Represents the status of an insurance policy.
+    # Reprezentuje status polisy ubezpieczeniowej.
     policy_status_name = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
-        # Returns the name of the policy status as a string representation of the object.
+        # Zwraca nazwę statusu polisy jako reprezentację obiektu w postaci ciągu znaków.
         return self.policy_status_name
 
     class Meta:
-        verbose_name = "Policy status"
-        verbose_name_plural = "Policy statuses"
+        verbose_name = "Status polisy"
+        verbose_name_plural = "Statusy polis"
 
 
 class PolicyType(models.Model):
-    # Represents the type of an insurance policy.
-    # Name of the policy type.
     policy_type_name = models.CharField(max_length=25)
 
     def __str__(self):
-        # Returns the name of the policy type as a string representation of the object.
         return self.policy_type_name
 
 
 class InsurancePolicy(models.Model):
-    # Represents an insurance policy.
     tuBase = models.ForeignKey(
         CustomTUBase,
-        verbose_name="Insurance Company",
-        on_delete=models.SET_NULL,  # Action to take when related object is deleted.
-        null=True,  # Allows null values in the database.
+        verbose_name="Towarzystwo ubezpieczeniowe",
+        on_delete=models.SET_NULL,
+        null=True,
     )
-    # Policy number, unique for each policy.
+
     policy_number = models.CharField(max_length=11, unique=True)
-    # Start date of policy validity.
+
     validity_start_date = models.DateField()
-    # End date of policy validity.
+
     validity_end_date = models.DateField()
     policy_creator = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
-    # Status of the policy, linked to PolicyStatus model, indicates the current state of the policy.
+
     status = models.ForeignKey(
         PolicyStatus,
-        verbose_name="Policies Status",
-        on_delete=models.SET_NULL,  # Action to take when related object is deleted.
-        null=True,  # Allows null values in the database.
+        verbose_name="Status polisy",
+        on_delete=models.SET_NULL,
+        null=True,
     )
-    # Type of the policy, linked to PolicyType model, specifies the type of the policy.
+
     policy_type = models.ForeignKey(
         PolicyType,
-        verbose_name="Policies Type",
-        on_delete=models.SET_NULL,  # Action to take when related object is deleted.
-        null=True,  # Allows null values in the database.
+        verbose_name="Typ polisy",
+        on_delete=models.SET_NULL,
+        null=True,
     )
 
     def __str__(self):
-        # Returns the policy number as a string representation of the object.
+
         return self.policy_number
-
-    def save(self, *args, **kwargs):
-        if self.tuBase:
-            company_name_prefix = slugify(self.tuBase.short_name)[:3].upper()
-            self.policy_number = f"{company_name_prefix}-{self.policy_number}"
-
-        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Insurance policy"
