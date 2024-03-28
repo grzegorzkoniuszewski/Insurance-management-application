d784e08ad2d22e901e0273507928103abd183d1f
grzegorzkoniuszewski <koniuszewski.grzegorz@gmail.com>
2024-03-28 13:10:25 +0100

    Removed content from models.py, removed comments from code
    Short description:
    - Removed content from models.py as the code has been moved to views.py.
    - Updated mainpage.html and views.py, removed unnecessary comments.

--- ima_display/views.py
+++ ima_display/views.py
@@ -1,17 +1,11 @@
 from django.views.generic import TemplateView
 
-class StartPage(TemplateView):
 
+class StartPage(TemplateView):
     template_name = 'mainpage.html'
 
-
     def get_context_data(self, **kwargs):
-
         context = super().get_context_data(**kwargs)
-
         current_user = self.request.user
-
         context['current_user'] = current_user
-
         return context
-
